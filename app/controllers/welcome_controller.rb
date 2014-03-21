9fb998e20d59c7e280a8e2ca51d654aec18d0cd0
K2 <k2@ubuntu.ubuntu-domain>
2014-03-21 10:43:49 +0530

    matching search result

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,19 @@ class WelcomeController < ApplicationController
 
   end
   def search
-  	@results = Book.where("name like '%#{params[:search_keyword][0]}%'") rescue []
+  	if params[:ajax] == "true"
+	  	search_string = []
+	  	search_string << "name like '%#{params[:search_keyword]}%'"
+	  	search_string << "price = #{params[:search_keyword].to_i}"
+	  	search_string << "author like '#{params[:search_keyword]}'"
+	  	@results = Book.where(search_string.join(" or ")) rescue []
+  		render :json =>  @results.map{|result| "#{result.name} @#{result.price}"}
+  	else
+  		search_string = []
+	  	search_string << "name like '%#{params[:search_keyword][0]}%'"
+	  	search_string << "price = #{params[:search_keyword][0].to_i}"
+	  	search_string << "author like '#{params[:search_keyword][0]}'"
+	  	@results = Book.where(search_string.join(" or ")) rescue []
+  	end
   end
 end
